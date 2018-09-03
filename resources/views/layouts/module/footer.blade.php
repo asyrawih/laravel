3e9c0c53b96be01288bb82775e731c2d43bcddff
hanan <hasyrawi@gmail.com>
2018-09-03 02:31:13 -0700

    as

---
+++ resources/views/layouts/module/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2018 <a href="http://hanan.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <p>Hanan asyrawi Rivai</p>
+    </div>
+</footer>
